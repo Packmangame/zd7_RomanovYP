a3388275855122869ba907954d57973ee267d918
ural-ECTS <ural@ects.ru>
2024-12-06 14:05:50 +0500

    s

---
+++ zd7_romanov/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace zd7_romanov
+{
+    public partial class MainPage : ContentPage
+    {
+        Dictionary<string,string> services;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ zd7_romanov/RegisterScreen.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace zd7_romanov
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegisterScreen : ContentPage
+    {
+        
+        public RegisterScreen()
+        {
+            InitializeComponent();
+
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(this.FindByName<Entry>("log").Text) || !string.IsNullOrEmpty(this.FindByName<Entry>("pas").Text))
+            {
+                if (this.FindByName<Entry>("log").Text == "ects" && this.FindByName<Entry>("pas").Text == "ects2024")
+                {
+                    await Navigation.PushAsync(new MainPage()); 
+                }
+                else
+                {
+                    DisplayAlert("Error", "Логин или пароль введенны неправильно", "Ok");
+                }
+
+            } else
+            {
+                DisplayAlert("Error","Заполните поля","Ok");
+            }
+
+        }
+    }
+
+    
+}
